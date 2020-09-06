91e4d7772b3be752a5171c2a5f85044e21ff2056
Allan Feldman <6374032+a-feld@users.noreply.github.com>
2020-09-06 11:11:08 -0400

    Ensure that socket is closed when errors occur.

--- src/pywreck/__init__.py
+++ src/pywreck/__init__.py
@@ -126,78 +126,81 @@ async def request(
     _reader, _writer = await asyncio.open_connection(host, port, ssl=ssl)
     reader, writer = _HttpReader(_reader, timeout), _HttpWriter(_writer, timeout)
 
-    request = f"{method} {uri} HTTP/1.1\r\n"
-    writer.write(request)
-
-    request_headers = {"host": host, "user-agent": f"pywreck/{__version__}"}
-    if payload:
-        request_headers["content-length"] = str(len(payload))
-
-    if headers:
-        request_headers.update(headers)
-
-    for header_name, header_value in request_headers.items():
-        writer.write(
-            "{header_name}:{header_value}\r\n".format(
-                header_name=header_name, header_value=header_value
-            )
-        )
-
-    # Finish request metadata section
-    writer.write("\r\n")
-
-    # Send payload
-    writer.write(payload)
-
-    response_line = await reader.readline()
-    status = int(response_line.split(" ", 2)[1])
-
-    response_headers: Dict[str, str] = {}
-    content_length = 0
-    chunked = False
-
-    while True:
-        header_line = await reader.readline()
-        header_line = header_line.rstrip()
-        if not header_line:
-            break
-
-        header_name, header_value = header_line.split(":", 1)
-        header_name = header_name.rstrip().lower()
-        header_value = header_value.lstrip()
-
-        if header_name in response_headers:
-            separator = "," if header_name != "set-cookie" else ";"
-            response_headers[header_name] += separator + header_value
-        else:
-            response_headers[header_name] = header_value
-
-    if method != "HEAD":
-        if "content-length" in response_headers:
-            content_length = int(response_headers["content-length"])
-
-        chunked = response_headers.get("transfer-encoding", "") == "chunked"
-
-    if chunked:
-        response_chunks = []
-        while True:
-            chunk_len_bytes = await reader.readuntil(b"\r\n")
-            content_length = int(chunk_len_bytes.rstrip(), 16)
-            if not content_length:
-                break
-            part = await reader.readexactly(content_length + 2)
-            response_chunks.append(part[:-2])
-
-        response_data = b"".join(response_chunks)
-    else:
-        response_data = await reader.readexactly(content_length)
-
-    writer.close()
     try:
-        await writer.wait_closed()
-    except asyncio.TimeoutError:
-        writer.transport.abort()
-    return Response(status, response_headers, response_data)
+        request = f"{method} {uri} HTTP/1.1\r\n"
+        writer.write(request)
+
+        request_headers = {"host": host, "user-agent": f"pywreck/{__version__}"}
+        if payload:
+            request_headers["content-length"] = str(len(payload))
+
+        if headers:
+            request_headers.update(headers)
+
+        for header_name, header_value in request_headers.items():
+            writer.write(
+                "{header_name}:{header_value}\r\n".format(
+                    header_name=header_name, header_value=header_value
+                )
+            )
+
+        # Finish request metadata section
+        writer.write("\r\n")
+
+        # Send payload
+        writer.write(payload)
+
+        response_line = await reader.readline()
+        status = int(response_line.split(" ", 2)[1])
+
+        response_headers: Dict[str, str] = {}
+        content_length = 0
+        chunked = False
+
+        while True:
+            header_line = await reader.readline()
+            header_line = header_line.rstrip()
+            if not header_line:
+                break
+
+            header_name, header_value = header_line.split(":", 1)
+            header_name = header_name.rstrip().lower()
+            header_value = header_value.lstrip()
+
+            if header_name in response_headers:
+                separator = "," if header_name != "set-cookie" else ";"
+                response_headers[header_name] += separator + header_value
+            else:
+                response_headers[header_name] = header_value
+
+        if method != "HEAD":
+            if "content-length" in response_headers:
+                content_length = int(response_headers["content-length"])
+
+            chunked = response_headers.get("transfer-encoding", "") == "chunked"
+
+        if chunked:
+            response_chunks = []
+            while True:
+                chunk_len_bytes = await reader.readuntil(b"\r\n")
+                content_length = int(chunk_len_bytes.rstrip(), 16)
+                if not content_length:
+                    break
+                part = await reader.readexactly(content_length + 2)
+                response_chunks.append(part[:-2])
+
+            response_data = b"".join(response_chunks)
+        else:
+            response_data = await reader.readexactly(content_length)
+
+        return Response(status, response_headers, response_data)
+
+    finally:
+        writer.close()
+        try:
+            await writer.wait_closed()
+        except asyncio.TimeoutError:
+            writer.transport.abort()
 
 
 get = functools.partial(request, "GET")
